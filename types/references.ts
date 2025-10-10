b6f07d3420fbb9867fb67b65ef8fef2efdf84436
komed3 <pk97@outlook.de>
2025-10-10 23:23:34 +0200

    define reference fields

--- types/references.ts
+++ types/references.ts
@@ -1,38 +1,25 @@
-import { Require, RequireEither } from './helper';
-
-interface BaseFields {
-    title?: string;
-    note?: string;
+interface ReferenceFields {
+    address?: string;
+    author?: string;
+    booktitle?: string;
+    chapter?: string;
+    edition?: string;
+    editor?: string;
+    howpublished?: string;
+    institution?: string;
+    isbn?: string;
+    journal?: string;
     month?: string;
+    note?: string;
+    number?: number | string;
+    organization?: string;
+    pages?: number | string;
+    publisher?: string;
+    school?: string;
+    series?: string;
+    title?: string;
+    type?: string;
+    volume?: number | string;
     year?: number | string;
 }
 
-interface PublicationFields extends BaseFields {
-    author?: string;
-    editor?: string;
-    publisher?: string;
-    journal?: string;
-    series?: string;
-    address?: string;
-    volume?: string;
-    number?: string;
-    edition?: string;
-    pages?: string;
-    organization?: string;
-    isbn?: string;
-    type?: string;
-}
-
-export type ReferenceArticle = Require< PublicationFields, 'author' | 'title' | 'journal' | 'year' > & {
-    type: 'article';
-    journal: string;
-};
-
-export type ReferenceBook = RequireEither< Require< PublicationFields, 'title' | 'publisher' | 'year' >, 'author', 'editor' > & {
-    type: 'book';
-};
-
-export type ReferenceBooklet = Require< PublicationFields, 'title' | 'author' | 'address' > & {
-    type: 'booklet';
-    howpublished?: string;
-};
