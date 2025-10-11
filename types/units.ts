1a7c6bfba78c0c1c3592e1cc7c7498737593652d
komed3 <pk97@outlook.de>
2025-10-11 23:47:30 +0200

    implement phys. dimension types

--- types/units.ts
+++ types/units.ts
@@ -1,3 +1,14 @@
-export type BaseDimension =
+export type SIDimensionType =
     'time' | 'length' | 'mass' | 'electricCurrent' | 'temperature' |
     'amountOfSubstance' | 'luminousIntensity';
+
+export type DerivedDimensionType =
+    'area' | 'volume' | 'velocity' | 'acceleration' | 'force' | 'energy' |
+    'power' | 'pressure' | 'enthalpy' | 'heatCapacity' | 'frequency' | 'density' |
+    'concentration' | 'charge' | 'potential' | 'resistance' | 'conductance' | 'capacitance' |
+    'magneticFlux' | 'magneticFieldStrength' | 'inductance' | 'radioactivity' |
+    'absorbedDose' | 'equivalentDose' | 'catalyticActivity';
+
+export type PhysicalDimensionType = SIDimensionType | DerivedDimensionType;
+
+type DimensionVector = readonly [ number, number, number, number, number, number, number ];
