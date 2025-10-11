eaa0d6c815f32df9e627e4cf87afd8a5a5882fc5
komed3 <pk97@outlook.de>
2025-10-11 09:48:19 +0200

    define reference fields

---
+++ types/references.ts
@@ -0,0 +1,32 @@
+import {} from './abstract';
+
+interface BibTeXFields {
+    address?: string;
+    author?: string;
+    booktitle?: string;
+    chapter?: string;
+    edition?: string;
+    editor?: string;
+    howpublished?: string;
+    institution?: string;
+    isbn?: string;
+    journal?: string;
+    month?: string;
+    note?: string;
+    number?: number | string;
+    organization?: string;
+    pages?: number | string;
+    publisher?: string;
+    school?: string;
+    series?: string;
+    title?: string;
+    type?: string;
+    volume?: number | string;
+    year?: number | string;
+}
+
+interface ExtraFields {
+    accessed?: string;
+    url?: string;
+    doi?: string;
+}
