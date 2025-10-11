a20fe34cd0a209df4286119b91028f4374f2dc7d
komed3 <pk97@outlook.de>
2025-10-12 00:08:34 +0200

    Update units.ts

--- types/units.ts
+++ types/units.ts
@@ -1,43 +1,51 @@
+/**
+ * Physical Dimensions and Units
+ */
+
+// SI base physical dimensions
 export type SIDimensionType =
     'time' | 'length' | 'mass' | 'electricCurrent' | 'temperature' |
     'amountOfSubstance' | 'luminousIntensity';
 
+// Derived physical dimensions
 export type DerivedDimensionType =
     'area' | 'volume' | 'velocity' | 'acceleration' | 'force' | 'energy' |
     'power' | 'pressure' | 'enthalpy' | 'heatCapacity' | 'frequency' | 'density' |
-    'concentration' | 'charge' | 'potential' | 'resistance' | 'conductance' | 'capacitance' |
-    'magneticFlux' | 'magneticFieldStrength' | 'inductance' | 'radioactivity' |
-    'absorbedDose' | 'equivalentDose' | 'catalyticActivity';
+    'concentration' | 'charge' | 'potential' | 'resistance' | 'conductance' |
+    'capacitance' | 'magneticFlux' | 'magneticFieldStrength' | 'inductance' |
+    'radioactivity' | 'absorbedDose' | 'equivalentDose' | 'catalyticActivity';
 
+// All physical dimensions
 export type PhysicalDimensionType = SIDimensionType | DerivedDimensionType;
 
+// A vector representing the powers of each base dimension in the order:
+// [ time, length, mass, electricCurrent, temperature, amountOfSubstance, luminousIntensity ]
 type PhysicalDimensionVector = readonly [ number, number, number, number, number, number, number ];
 
-interface PhysicalDimension {
-    readonly symbol: string;
-    readonly name: string;
-    readonly vector: PhysicalDimensionVector;
-}
-
-interface SIConversion {
-    readonly factor: number;
-    readonly offset?: number;
-}
-
+// Interface describing a physical unit
 interface PhysicalUnit< T extends PhysicalDimensionType > {
     readonly symbol: string;
     readonly dimension: T;
     readonly name?: string;
     readonly isBase?: boolean;
-    readonly conversion?: SIConversion;
+    readonly conversion?: {
+        readonly factor: number;
+        readonly offset?: number;
+    };
 }
 
+// Collection of units for a specific physical dimension
 interface QuantityUnits< T extends PhysicalDimensionType > {
-    readonly dimension: PhysicalDimension;
+    readonly dimension: {
+        readonly symbol: string;
+        readonly name: string;
+        readonly vector: PhysicalDimensionVector;
+    };
     readonly baseUnit: string;
     readonly units: Record< string, PhysicalUnit< T > >;
 }
 
-export type PhysicalQuantities = {
+// Collection of physical quantities
+export type PhysicalQuantities = Partial< {
     readonly [ K in PhysicalDimensionType ]: QuantityUnits< K >;
-};
+} >;
