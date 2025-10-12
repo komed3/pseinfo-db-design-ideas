8374a59556c8b832f1c05c39f7e327a018718b53
komed3 <pk97@outlook.de>
2025-10-12 18:42:05 +0200

    implement value types

--- types/properties.ts
+++ types/properties.ts
@@ -1,38 +1,51 @@
-import { RequireAtLeastOne } from './abstract';
+import { RequireAtLeastOne, StrictSubset } from './abstract';
+import { FormId } from './forms';
 import { RefId } from './references';
 import { Uncertainty } from './uncertainty';
-import { PhysicalDimension, PhysicalQuantities, UnitReference } from './units';
+import { PhysicalDimension, UnitId } from './units';
 
 export type ValueOrigin =
     'measured' | 'calculated' | 'estimated' | 'theoretical' | 'experimental' | 'literature' |
     'recommended' | 'critical' | 'standard' | 'extrapolated' | 'interpolated';
 
-interface BaseValue< T, D extends PhysicalDimension = PhysicalDimension > {
+export type ValueType = 'single' | 'array' | 'range' | 'coupled';
+
+interface ConditionValue {
+  value: number;
+  unit_ref?: UnitId;
+  uncertainty?: Uncertainty;
+  note?: string;
+}
+
+type Conditions = Partial< Record< PhysicalDimension, ConditionValue | ConditionValue[] > >;
+
+interface BaseFields< T extends ValueType > {
     type: T;
     origin?: ValueOrigin;
-    form_refs?: any;
-    unit_ref?: UnitReference< PhysicalQuantities, D >;
-    conditions?: any;
+    form_refs?: FormId[];
+    uncertainty?: Uncertainty;
+    conditions?: Conditions;
     references?: RefId[];
     note?: string;
 }
 
-export interface SingleValue< D extends PhysicalDimension = PhysicalDimension > extends BaseValue< 'single', D > {
-    value: number;
-    uncertainty?: Uncertainty;
-}
-
-export interface ArrayValue< D extends PhysicalDimension = PhysicalDimension > extends BaseValue< 'array', D > {
-    values: number[];
-    uncertainty?: Uncertainty;
-}
-
-export interface RangeValue< D extends PhysicalDimension = PhysicalDimension > extends BaseValue< 'range', D > {
+interface ValueFields {
     value?: number;
-    uncertainty?: Uncertainty;
-    range: RequireAtLeastOne< Record< 'lower' | 'upper', {
+    values?: number[];
+    range?: RequireAtLeastOne< Record< 'lower' | 'upper', {
         value: number;
         uncertainty?: Uncertainty;
         inclusive?: boolean;
     } > >;
+    unit_ref?: UnitId;
 }
+
+export type SingleValue = BaseFields< 'single' > & StrictSubset< ValueFields, 'value', 'unit_ref' >;
+
+export type ArrayValue = BaseFields< 'array' > & StrictSubset< ValueFields, 'values', 'unit_ref' >;
+
+export type RangeValue = BaseFields< 'range' > & StrictSubset< ValueFields, 'value' | 'range', 'unit_ref' >;
+
+export type CoupledValue = BaseFields< 'coupled' > & {
+    properties: Partial< Record< PhysicalDimension, SingleValue | ArrayValue | RangeValue > >
+};
