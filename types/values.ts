7c08141c26f9493293758810ee8825c1df1a32f7
komed3 <pk97@outlook.de>
2025-10-12 18:48:11 +0200

    minor changes

--- types/values.ts
+++ types/values.ts
@@ -1,15 +1,20 @@
+/**
+ * Complex Physical Values
+ */
+
 import { RequireAtLeastOne, StrictSubset } from './abstract';
 import { FormId } from './forms';
 import { RefId } from './references';
 import { Uncertainty } from './uncertainty';
 import { PhysicalDimension, UnitId } from './units';
 
-export type ValueOrigin =
-    'measured' | 'calculated' | 'estimated' | 'theoretical' | 'experimental' | 'literature' |
-    'recommended' | 'critical' | 'standard' | 'extrapolated' | 'interpolated';
+// Origin of the value
+export type ValueOrigin = 'measured' | 'calculated' | 'estimated' | 'theoretical' | 'experimental';
 
+// Type of value
 export type ValueType = 'single' | 'array' | 'range' | 'coupled';
 
+// Value under specific conditions
 interface ConditionValue {
   value: number;
   unit_ref?: UnitId;
@@ -19,6 +24,7 @@ interface ConditionValue {
 
 type Conditions = Partial< Record< PhysicalDimension, ConditionValue | ConditionValue[] > >;
 
+// Base fields for all value types
 interface BaseFields< T extends ValueType > {
     type: T;
     origin?: ValueOrigin;
@@ -29,6 +35,7 @@ interface BaseFields< T extends ValueType > {
     note?: string;
 }
 
+// Fields specific to value types
 interface ValueFields {
     value?: number;
     values?: number[];
@@ -40,12 +47,19 @@ interface ValueFields {
     unit_ref?: UnitId;
 }
 
+// Single value type
 export type SingleValue = BaseFields< 'single' > & StrictSubset< ValueFields, 'value', 'unit_ref' >;
 
+// Array of values type
 export type ArrayValue = BaseFields< 'array' > & StrictSubset< ValueFields, 'values', 'unit_ref' >;
 
+// Range of values type
 export type RangeValue = BaseFields< 'range' > & StrictSubset< ValueFields, 'value' | 'range', 'unit_ref' >;
 
+// Coupled values type (e.g., multiple related properties)
 export type CoupledValue = BaseFields< 'coupled' > & {
     properties: Partial< Record< PhysicalDimension, SingleValue | ArrayValue | RangeValue > >
 };
+
+// Union type for all value types
+export type Value = SingleValue | ArrayValue | RangeValue | CoupledValue;
