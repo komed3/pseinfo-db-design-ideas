e7dc57fcbb798bcb2292fc76d3ef83a187d27f8e
komed3 <pk97@outlook.de>
2025-10-12 17:07:16 +0200

    Update uncertainty.ts

--- types/uncertainty.ts
+++ types/uncertainty.ts
@@ -2,40 +2,63 @@
  * Physical Uncertainty
  */
 
-import { LiteralUnion } from './abstract';
+import { LiteralUnion, StrictSubset } from './abstract';
 
 // Types of uncertainty
 export type UncertaintyType = 'absolute' | 'relative' | 'asymmetrical' | 'statistical' | 'estimated' | 'unknown';
 
-// Base interface for uncertainty types
-interface BaseUncertainty< T extends UncertaintyType > {
+// Base fields for all uncertainty types
+interface BaseFields< T extends UncertaintyType > {
     type: T;
     confidence?: number;
     note?: string;
 }
 
-// Specific uncertainty type interfaces
-interface AbsoluteUncertainty extends BaseUncertainty< 'absolute' > {
-    deviation: number;
-}
-
-interface RelativeUncertainty extends BaseUncertainty< 'relative' > {
-    relative: number;
-}
-
-interface AsymmetricalUncertainty extends BaseUncertainty< 'asymmetrical' > {
-    plus: number;
-    minus: number;
-}
-
-interface StatisticalUncertainty extends BaseUncertainty< 'statistical' > {
-    deviation: number;
+// Fields specific to different uncertainty types
+interface UncertaintyFields {
+    deviation?: number;
+    relative?: number;
+    plus?: number;
+    minus?: number;
     statistical: LiteralUnion< 'stddev' | 'stderr' | 'confidence95' >;
 }
 
-type EstimatedUncertainty = BaseUncertainty< 'estimated' >;
+// Specific uncertainty type definitions
+export type AbsoluteUncertainty =
+    BaseFields< 'absolute' > &
+    StrictSubset<
+        UncertaintyFields,
+        'deviation',
+        never
+    >;
 
-type UnknownUncertainty = BaseUncertainty< 'unknown' >;
+export type RelativeUncertainty =
+    BaseFields< 'relative' > &
+    StrictSubset<
+        UncertaintyFields,
+        'relative',
+        never
+    >;
+
+export type AsymmetricalUncertainty =
+    BaseFields< 'asymmetrical' > &
+    StrictSubset<
+        UncertaintyFields,
+        'plus' | 'minus',
+        never
+    >;
+
+export type StatisticalUncertainty =
+    BaseFields< 'statistical' > &
+    StrictSubset<
+        UncertaintyFields,
+        'deviation' | 'statistical',
+        never
+    >;
+
+export type EstimatedUncertainty = BaseFields< 'estimated' >;
+
+export type UnknownUncertainty = BaseFields< 'unknown' >;
 
 // Union type for all uncertainty types
 export type Uncertainty =
