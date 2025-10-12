1a0d2cf1b03aadae5a96b49f3b60aa5a70767755
komed3 <pk97@outlook.de>
2025-10-12 14:37:27 +0200

    increase type safety

--- types/properties.ts
+++ types/properties.ts
@@ -7,21 +7,22 @@ export type ValueOrigin =
     'measured' | 'calculated' | 'estimated' | 'theoretical' | 'experimental' | 'literature' |
     'recommended' | 'critical' | 'standard' | 'extrapolated' | 'interpolated';
 
-interface BaseValue< T extends PhysicalDimension = PhysicalDimension > {
+interface BaseValue< T, D extends PhysicalDimension = PhysicalDimension > {
+    type: T;
     origin?: ValueOrigin;
     form_refs?: any;
-    unit_ref?: UnitReference< PhysicalQuantities, T >;
+    unit_ref?: UnitReference< PhysicalQuantities, D >;
     conditions?: any;
     references?: Array< keyof References >;
     note?: string;
 }
 
-export interface SingleValue< T extends PhysicalDimension = PhysicalDimension > extends BaseValue< T > {
+export interface SingleValue< D extends PhysicalDimension = PhysicalDimension > extends BaseValue< 'single', D > {
     value: number;
     uncertainty?: Uncertainty;
 }
 
-export interface RangeValue< T extends PhysicalDimension = PhysicalDimension > extends BaseValue< T > {
+export interface RangeValue< D extends PhysicalDimension = PhysicalDimension > extends BaseValue< 'range', D > {
     value?: number;
     uncertainty?: Uncertainty;
     range: RequireAtLeastOne< Record< 'lower' | 'upper', {
