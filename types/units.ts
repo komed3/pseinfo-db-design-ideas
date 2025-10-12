841a7cefde9fe7ceea7cf21d90581408d06bc506
komed3 <pk97@outlook.de>
2025-10-12 10:06:37 +0200

    fixes + improvements

--- types/units.ts
+++ types/units.ts
@@ -3,12 +3,12 @@
  */
 
 // SI base physical dimensions
-export type SIDimensionType =
+export type SIDimension =
     'time' | 'length' | 'mass' | 'electricCurrent' | 'temperature' |
     'amountOfSubstance' | 'luminousIntensity';
 
 // All physical dimensions
-export type PhysicalDimensionType = SIDimensionType |
+export type PhysicalDimension = SIDimension |
     'area' | 'volume' | 'velocity' | 'acceleration' | 'force' | 'energy' |
     'power' | 'pressure' | 'enthalpy' | 'heatCapacity' | 'frequency' | 'density' |
     'concentration' | 'charge' | 'potential' | 'resistance' | 'conductance' |
@@ -17,10 +17,10 @@ export type PhysicalDimensionType = SIDimensionType |
 
 // A vector representing the powers of each base dimension in the order:
 // [ time, length, mass, electricCurrent, temperature, amountOfSubstance, luminousIntensity ]
-type PhysicalDimensionVector = [ number, number, number, number, number, number, number ];
+type DimensionVector = [ number, number, number, number, number, number, number ];
 
 // Interface describing a physical unit
-interface PhysicalUnit< T extends PhysicalDimensionType > {
+interface PhysicalUnit< T extends PhysicalDimension > {
     symbol: string;
     dimension: T;
     name?: string;
@@ -32,18 +32,18 @@ interface PhysicalUnit< T extends PhysicalDimensionType > {
 }
 
 // Collection of units for a specific physical dimension
-interface QuantityUnits< T extends PhysicalDimensionType > {
+interface QuantityUnits< T extends PhysicalDimension > {
     dimension: {
         symbol: string;
         name: string;
-        vector: PhysicalDimensionVector;
+        vector: DimensionVector;
     };
     baseUnit: string;
     units: Record< string, PhysicalUnit< T > >;
 }
 
 // Collection of physical quantities
-export type PhysicalQuantities = Partial< { [ K in PhysicalDimensionType ]: QuantityUnits< K > } >;
+export type PhysicalQuantities = { [ K in PhysicalDimension ]: QuantityUnits< K > };
 
 // Get the keys of units for a specific physical quantity Q in T
 type UnitsForQuantity< T extends PhysicalQuantities, Q extends keyof T > =
