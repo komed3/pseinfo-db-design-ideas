b4e3921809cb21d9f56cdd0e520fb7a1df3ff970
komed3 <pk97@outlook.de>
2025-10-13 16:02:24 +0200

    Update classification.ts

--- types/element/classification.ts
+++ types/element/classification.ts
@@ -2,12 +2,28 @@
  * Element Classification Property Types
  */
 
-import { MapPropertyDefinition } from '../property';
+import { Distinct, MapPropertyDefinition } from '../property';
 
 // Classification property names
-export type ClassificationProperty = '';
+export type ClassificationProperty =
+    'symbol' | 'atomicNumber' | 'block' | 'column' | 'group' | 'period' | 'set' | 'phase' |
+    'naturalOccurrence' | 'goldschmidt' | 'superconductivity' | 'radioactive';
 
 // Collection of classification properties
 export type ClassificationCollection = MapPropertyDefinition< {
-    //
+    symbol: Distinct< string >;
+    atomicNumber: Distinct< number >;
+    block: Distinct< 's' | 'p' | 'd' | 'f' >;
+    column: Distinct< number >;
+    group: Distinct< number >;
+    period: Distinct< number >;
+    set: Distinct<
+        'nonMetal' | 'nobleGas' | 'alkaliMetal' | 'alkalineEarthMetal' | 'metalloid' | 'halogen' |
+        'metal' | 'transitionMetal' | 'lanthanoide' | 'actinoide' | undefined
+    >;
+    phase: Distinct< 'solid' | 'gas' | 'liquid' | undefined >;
+    naturalOccurrence: Distinct< 'primordial' | 'decayProduct' | 'synthetic' >;
+    goldschmidt: Distinct< 'atmophile' | 'chalcophile' | 'lithophile' | 'siderophile' | 'synthetic' >;
+    superconductivity: Distinct< 'none' | 'normal' | 'special' | undefined >;
+    radioactive: Distinct< boolean >;
 } >;
