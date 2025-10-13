3d0e915ec899fbddfdee3030525f5cffbfd20a0b
komed3 <pk97@outlook.de>
2025-10-13 12:20:50 +0200

    add generic property collection mapper

--- types/property.ts
+++ types/property.ts
@@ -1,30 +1,33 @@
 import { OneOrMany } from './abstract';
-import { ArrayValue, CoupledNumberValue, CoupledValue, NumberValue, PrimitiveValue, RangeValue, SingleValue, Value } from './value';
+import { ArrayValue, CoupledNumberValue, CoupledValue, PrimitiveValue, RangeValue, SingleValue } from './value';
 
+// Basic property types
 export type PrimitiveProperty = OneOrMany< PrimitiveValue >;
-
 export type SingleProperty = OneOrMany< SingleValue >;
-
 export type ArrayProperty = OneOrMany< ArrayValue >;
-
 export type RangeProperty = OneOrMany< RangeValue >;
-
-export type CoupledProperty = OneOrMany< CoupledValue >;
-
 export type CoupledNumberProperty = OneOrMany< CoupledNumberValue >;
+export type CoupledProperty = OneOrMany< CoupledValue >;
+export type NumberProperty = OneOrMany< SingleProperty | ArrayProperty | RangeProperty | CoupledNumberProperty >;
+export type Property = OneOrMany< NumberProperty | PrimitiveProperty | CoupledProperty >;
 
-export type NumberProperty = OneOrMany< NumberValue >;
+// Grouped property types
+export type NumberPropertyGroup< Names extends string > = { [ K in Names ]?: NumberProperty };
+export type PropertyGroup< Names extends string > = { [ K in Names ]?: Property };
 
-export type Property = OneOrMany< Value >;
+// Utility types
+export type Single< T extends Property > = T;
+export type Group< T extends Record< string, Property | undefined > > = T;
 
-export type NumberPropertyGroup< Properties extends string > = {
-    [ P in Properties ]?: NumberProperty;
-};
-
-export type PropertyGroup< Properties extends string > = {
-    [ P in Properties ]?: Property;
-};
-
-export type PropertyCollection< Groups extends string, Properties extends string > = {
-    [ G in Groups ]?: NumberProperty | Property | NumberPropertyGroup< Properties > | PropertyGroup< Properties >;
+// Generic property collection mapper
+export type MapPropertyDefinition< T > = {
+    [ K in keyof T ]?: T[ K ] extends NumberProperty | PrimitiveProperty | Property
+        ? T[ K ]
+        : T[ K ] extends Record< string, any >
+        ? T[ K ] extends { [ key: string ]: NumberProperty | undefined }
+            ? NumberPropertyGroup< keyof T[ K ] & string >
+            : T[ K ] extends { [ key: string ]: Property | undefined }
+            ? PropertyGroup< keyof T[ K ] & string >
+            : never
+        : never;
 };
