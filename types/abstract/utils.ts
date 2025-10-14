b3418573f6a8c08719647007478dcba9c00e8664
komed3 <pk97@outlook.de>
2025-10-14 11:15:13 +0200

    Update utils.ts

--- types/abstract/utils.ts
+++ types/abstract/utils.ts
@@ -1,20 +1,6 @@
 // Primitive types in TypeScript
 export type Primitive = string | number | boolean | symbol | null | undefined;
 
-// Require at least one property from K, all other properties remain unchanged
-export type RequireAtLeastOne< T extends object, K extends keyof T = keyof T > =
-    Pick< T, Exclude< keyof T, K > > &
-    { [ O in K ]: RequireFrom< T, O > & ExtractFrom< T, Exclude< K, O > > }[ K ];
-
-// Require exactly one property from K, all other properties remain unchanged
-export type RequireOnlyOne< T extends object, K extends keyof T = keyof T > =
-    { [ P in K ]: RequireFrom< T, P > & Partial< Record< Exclude< K, P >, undefined > > }[ K ] &
-    Omit< T, K >;
-
-// Require exactly one property from A or B (XOR), all other properties remain unchanged
-export type RequireExactlyOne< T, K extends keyof T = keyof T > =
-    { [ P in K ]: RequireFrom< T, P > & { [ Q in Exclude< K, P > ]?: never } & Omit< T, K > }[ K ];
-
 // Pick properties K from T, making them optional
 export type ExtractFrom< T, K extends keyof T > = Partial< Pick< T, K > >;
 
@@ -23,3 +9,12 @@ export type RequireFrom< T, K extends keyof T > = Required< Pick< T, K > >;
 
 // Require properties R and make properties O optional, all other properties remain unchanged
 export type StrictSubset< T extends object, R extends keyof T, O extends keyof T > = RequireFrom< T, R > & ExtractFrom< T, O >;
+
+// Require exactly one property from A or B (XOR), all other properties remain unchanged
+export type RequireExactlyOne< T, K extends keyof T = keyof T > =
+    { [ P in K ]: RequireFrom< T, P > & { [ Q in Exclude< K, P > ]?: never } & Omit< T, K > }[ K ];
+
+// Require at least one property from K, all other properties remain unchanged
+export type RequireAtLeastOne< T extends object, K extends keyof T = keyof T > =
+    Pick< T, Exclude< keyof T, K > > &
+    { [ O in K ]: RequireFrom< T, O > & ExtractFrom< T, Exclude< K, O > > }[ K ];
