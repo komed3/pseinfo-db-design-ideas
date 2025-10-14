f9bdbbc00bc437112a3b7a816f21ec6e6ef2aa05
komed3 <pk97@outlook.de>
2025-10-14 15:50:55 +0200

    define some phys. properties

--- types/element/physics.ts
+++ types/element/physics.ts
@@ -1,1 +1,28 @@
-export type PhysicsCollection = {};
+import { Collection, Group, Single } from '../abstract/collection';
+import { CoupledNumberProperty, NumberProperty, PrimitiveProperty } from '../abstract/property';
+
+export type PhysicsCollection = Collection< {
+    density?: Single< NumberProperty< 'density' > >;
+    phase?: Single< PrimitiveProperty< string > >;
+    soundSpeed?: Single< NumberProperty< 'velocity' > >;
+    temperature?: Group< {
+        meltingTemp?: Single< NumberProperty< 'temperature' > >;
+        boilingTemp?: Single< NumberProperty< 'temperature' > >;
+        liquidTemp?: Single< NumberProperty< 'temperature' > >;
+        transitionTemp?: Single< NumberProperty< 'temperature' > >;
+        sublimationTemp?: Single< NumberProperty< 'temperature' > >;
+        criticalPoint?: Single< CoupledNumberProperty< 'temperature' | 'pressure' > >;
+        triplePoint?: Single< CoupledNumberProperty< 'temperature' | 'pressure' > >;
+    } >;
+    enthalpy?: Group< {
+        fusionEnthalpy?: Single< NumberProperty< 'enthalpy' > >;
+        vaporisationEnthalpy?: Single< NumberProperty< 'enthalpy' > >;
+        atomizationEnthalpy?: Single< NumberProperty< 'enthalpy' > >;
+    } >;
+    heat?: Group< {
+        heatCapacity?: Single< NumberProperty< 'heatCapacity' > >;
+        thermalConductivity?: Single< NumberProperty< 'thermalConductivity' > >;
+        thermalExpansion?: Single< NumberProperty< 'length' | 'area' | 'volume' > >;
+        workFunction?: Single< NumberProperty< 'energy' > >;
+    } >;
+} >;
