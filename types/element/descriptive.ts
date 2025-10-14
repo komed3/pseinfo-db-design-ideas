ce3451b93d42600c1534f701c8566014753e9363
komed3 <pk97@outlook.de>
2025-10-14 20:01:25 +0200

    names + registry

--- types/element/descriptive.ts
+++ types/element/descriptive.ts
@@ -1,5 +1,19 @@
 // not yet implemented
 
-import { Collection } from '../abstract/collection';
+import { Collection, FormGroup, LangGroup, Single } from '../abstract/collection';
+import { PrimitiveProperty } from '../abstract/property';
 
-export type DescriptiveCollection = Collection< {} >;
+export type DescriptiveCollection = Collection< {
+    names: LangGroup< 'en' | 'la' >;
+    appearance?: LangGroup;
+    registry: FormGroup< {
+        cas: Single< PrimitiveProperty< string > >;
+        inChI: Single< PrimitiveProperty< string > >;
+        inChIkey: Single< PrimitiveProperty< string > >;
+        cid: Single< PrimitiveProperty< string > >;
+        echa?: Single< PrimitiveProperty< string > >;
+        eg?: Single< PrimitiveProperty< string > >;
+        wikidata?: Single< PrimitiveProperty< string > >;
+        rtecs?: Single< PrimitiveProperty< string > >;
+    } >;
+} >;
