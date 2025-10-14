433fc609515f9af17385576ed470538bd9de6518
komed3 <pk97@outlook.de>
2025-10-14 16:52:39 +0200

    add chem. collection

--- types/element/chemistry.ts
+++ types/element/chemistry.ts
@@ -1,3 +1,20 @@
-import { Collection } from '../abstract/collection';
+import { Collection, Group, Single } from '../abstract/collection';
+import { NumberProperty, PrimitiveProperty } from '../abstract/property';
 
-export type ChemistryCollection = Collection< {} >;
+export type ChemistryCollection = Collection< {
+    molarMass?: Single< NumberProperty< 'molarMass' > >;
+    basicity?: Single< PrimitiveProperty< string > >;
+    electronegativity?: Group< {
+        paulingNegativity?: Single< PrimitiveProperty< number > >;
+        sandersonNegativity?: Single< PrimitiveProperty< number > >;
+        allredNegativity?: Single< PrimitiveProperty< number > >;
+        mullikenNegativity?: Single< PrimitiveProperty< number > >;
+        allenNegativity?: Single< PrimitiveProperty< number > >;
+        ghoshGuptaNegativity?: Single< NumberProperty< 'energy' > >;
+        nagleNegativity?: Single< PrimitiveProperty< number > >;
+        pearsonNegativity?: Single< NumberProperty< 'energy' > >;
+    } >;
+    oxidationState?: Single< PrimitiveProperty< string > >;
+    oxideCharacter?: Single< PrimitiveProperty< string > >;
+    standardPotential?: Single< NumberProperty< 'electricPotential' > >;
+} >;
