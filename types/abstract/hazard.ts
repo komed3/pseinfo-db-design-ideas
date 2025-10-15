291d3f6588c1e5587097b4ba4ca576c9df2d60dc
komed3 <pk97@outlook.de>
2025-10-15 23:45:43 +0200

    define consts for new hazard module

---
+++ types/abstract/hazard.ts
@@ -0,0 +1,26 @@
+import {} from './collection';
+
+// Hazard signal words
+export const SignalWord = [ 'danger', 'warning' ] as const;
+export type SignalWord = ( typeof SignalWord )[ number ];
+
+// GHS pictograms
+export const GHSPictogram = [
+    'explosive', 'flammable', 'oxidizing', 'compressedGas', 'corrosive',
+    'toxic', 'harmful', 'healthHazard', 'environmentalHazard'
+] as const;
+
+export type GHSPictogram = ( typeof GHSPictogram )[ number ];
+
+// GHS hazard classes
+export const GHSClass = [ '01', '02', '03', '04', '05', '06', '07', '08', '09' ] as const;
+export type GHSClass = ( typeof GHSClass )[ number ];
+
+// ADR hazard classes
+export const ADRClass = [
+    '1', '1.4', '1.5D', '1.6N', '2.1', '2.2', '2.3', '3', '4.1', '4.2',
+    '4.3', '5.1', '5.2', '6.1', '6.2', '7A', '7B', '7C', '7E', '8', '9',
+    '9A', 'HOT', 'POL'
+] as const;
+
+export type ADRClass = ( typeof ADRClass )[ number ];
