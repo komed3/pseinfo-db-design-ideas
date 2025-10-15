124a33dab3ee3b9e59f0ed9b678952a2edb35aaa
komed3 <pk97@outlook.de>
2025-10-15 19:35:03 +0200

    add collection types

---
+++ types/abstract/collection.ts
@@ -0,0 +1,18 @@
+import { Property } from './property';
+import { LiteralUnion } from './utils';
+
+// Utility types
+export type Single< T extends Property > = T;
+export type Distinct< T = unknown > = T;
+export type Group< T extends Record< string, Single< Property > | Distinct< unknown > > > = T;
+export type LangGroup< L extends string = 'en' > = Group< {
+    [ K in LiteralUnion< L > ]: Distinct< string >;
+} >;
+
+// Generic collection mapper
+export type Collection< T > = { [ K in keyof T ]:
+    T[ K ] extends Single< infer P > ? P :
+    T[ K ] extends Group< infer G > ? { [ GK in keyof G ]: Collection< G[ GK ] > } :
+    T[ K ] extends Distinct< infer D > ? Distinct< D > :
+    T[ K ] extends Property ? T[ K ] :
+    never };
