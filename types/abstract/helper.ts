f1f27d5a75ec8626a6c67e60aca771920859ddee
komed3 <pk97@outlook.de>
2025-10-15 22:46:06 +0200

    add consts

--- types/abstract/helper.ts
+++ types/abstract/helper.ts
@@ -1,3 +1,23 @@
 // Phase of matter
 export const Phase = [ 'solid', 'gaseous', 'liquid' ] as const;
 export type Phase = ( typeof Phase )[ number ];
+
+// Common element sets/groups
+export const ElementSet = [
+    'nonMetal', 'nobleGas', 'alkaliMetal', 'alkalineEarthMetal', 'metalloid', 'halogen',
+    'metal', 'transitionMetal', 'lanthanoide', 'actinoide'
+] as const;
+
+export type ElementSet = ( typeof ElementSet )[ number ];
+
+// Natural occurrence types
+export const NaturalOccurrence = [ 'primordial', 'decayProduct', 'synthetic' ] as const;
+export type NaturalOccurrence = ( typeof NaturalOccurrence )[ number ];
+
+// Goldschmidt classification types
+export const Goldschmidt = [ 'atmophile', 'chalcophile', 'lithophile', 'siderophile', 'synthetic' ] as const;
+export type Goldschmidt = ( typeof Goldschmidt )[ number ];
+
+// Superconductivity types
+export const Superconductivity = [ 'none', 'normal', 'special' ] as const;
+export type Superconductivity = ( typeof Superconductivity )[ number ];
