afe9a05072c24391d7c4c419ea93bc0d080f3c13
komed3 <pk97@outlook.de>
2025-10-16 09:24:25 +0200

    fix ADR classes

--- types/abstract/hazard.ts
+++ types/abstract/hazard.ts
@@ -27,9 +27,9 @@ export type WHMISClass = ( typeof WHMISClass )[ number ];
 
 // ADR hazard classes
 export const ADRClass = [
-    '1', '1.4', '1.5D', '1.6N', '2.1', '2.2', '2.3', '3', '4.1', '4.2',
-    '4.3', '5.1', '5.2', '6.1', '6.2', '7A', '7B', '7C', '7E', '8', '9',
-    '9A', 'HOT', 'POL'
+    '1', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '2.1', '2.2', '2.3',
+    '3', '4.1', '4.2', '4.3', '5.1', '5.2', '6.1', '6.2', '7A', '7B', '7C',
+    '7E', '8', '9', '9A', 'HOT', 'POL'
 ] as const;
 
 export type ADRClass = ( typeof ADRClass )[ number ];
