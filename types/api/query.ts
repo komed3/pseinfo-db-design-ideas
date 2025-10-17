0630552cac7b2e7a33959a8c85d77303593f0c76
komed3 <pk97@outlook.de>
2025-10-17 11:49:19 +0200

    implement query builder

--- types/api/query.ts
+++ types/api/query.ts
@@ -1,6 +1,6 @@
 import { QueryCondition } from './condition';
 import { PaginationOptions, SortOptions } from './modifier';
-import { SelectOptions } from './select';
+import { FieldPaths, SelectOptions } from './select';
 
 // Query for a single collection with all options
 export type CollectionQuery< T > = QueryCondition< T > & PaginationOptions & SortOptions< T > & SelectOptions< T > & {
@@ -35,3 +35,15 @@ export type Query< DB > = {
 export type ElementQuery< EC > = {
     [ K in keyof EC ]?: CollectionQuery< EC[ K ] >;
 } & CollectionQuery< EC extends Record< string, infer E > ? E : never >;
+
+// Query builder interface for fluent API
+export interface QueryBuilder< T > {
+    where( condition: QueryCondition< T > ): QueryBuilder< T >;
+    select< F extends FieldPaths< T > >( ...fields: F[] ): QueryBuilder< T >;
+    exclude< F extends FieldPaths< T > >( ...fields: F[] ): QueryBuilder< T >;
+    sort( options: SortOptions< T >[ '$sort' ] ): QueryBuilder< T >;
+    limit( n: number ): QueryBuilder< T >;
+    skip( n: number ): QueryBuilder< T >;
+    distinct( field: keyof T ): QueryBuilder< T >;
+    build(): CollectionQuery< T >;
+}
