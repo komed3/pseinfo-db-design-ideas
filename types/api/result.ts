c04451a1afb7a3be75f4d14fb7d8a16353297fd0
komed3 <pk97@outlook.de>
2025-10-19 00:36:42 +0200

    implement the QueryResult type

--- types/api/result.ts
+++ types/api/result.ts
@@ -1,11 +1,52 @@
+import { HasSelection, CollectionValue, QueryableCollection } from './helper';
 import { Query } from './query';
+import { DataBase } from '../database';
+import { DeepPartial } from '../abstract/utils';
 
-// Result type for a query
+// Result type definitions for queries
+type FieldWithSelection< T, S > = NonNullable< T > extends readonly any[]
+    ? NonNullable< T >
+    : string extends keyof NonNullable< T >
+        ? Record< string, SelectedResult< CollectionValue< NonNullable< T > >, S > >
+        : SelectedResult< NonNullable< T >, S >;
+
+type FieldDefault< T > = NonNullable< T > extends readonly any[]
+    ? NonNullable< T >
+    : string extends keyof NonNullable< T >
+        ? Record< string, SelectedResult< CollectionValue< NonNullable< T > >, true > >
+        : NonNullable< T >;
+
+// Main selected result type
+type SelectedResult< T, S > =
+    S extends true ? T :
+    S extends false | undefined ? never :
+    S extends Record< string | number, any > ? (
+        HasSelection< S > extends true ? {
+            [ K in keyof S & keyof T as S[ K ] extends false | undefined ? never : K ]:
+                FieldWithSelection< T[ K ], S[ K ] >;
+        } : {
+            [ K in keyof T as K extends keyof S ? ( S[ K ] extends false ? never : K ) : K ]:
+                K extends keyof S ? FieldWithSelection< T[ K ], S[ K ] > : FieldDefault< T[ K ] >;
+        }
+    ) : never;
+
+// Result type for the $select part of a query
+type QuerySelectResult< S extends { [ k: string ]: any } > = {
+    [ C in keyof S & QueryableCollection ]: S[ C ] extends true
+        ? Record< string, DeepPartial< CollectionValue< DataBase[ C ] > > >
+        : S[ C ] extends Record< string, any >
+            ? Record< string, SelectedResult< DeepPartial< CollectionValue< DataBase[ C ] > >, S[ C ] > >
+            : never;
+};
+
+// Final query result type
 export type QueryResult< Q extends Query > = {
     query: Q;
-    result: any; // not yet implemented
-    references: any; // not yet implemented
-    units: any; // not yet implemented
+    result: {
+        data: QuerySelectResult< Q[ '$select' ] >;
+        references: any; // not yet implemented
+        units: any; // not yet implemented
+    };
     metadata: {
         total: number;
         limit: number;
