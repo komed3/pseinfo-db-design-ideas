2e9a11404897e3de761f1bdac2facbfaf8423079
komed3 <pk97@outlook.de>
2025-10-18 09:28:26 +0200

    Update sort.ts

--- types/api/sort.ts
+++ types/api/sort.ts
@@ -1,10 +1,10 @@
 import { DataBasePaths } from './path';
 
 // Possible sorting flags for queries
-export const QuerySortFlags = [ 1, -1, 'asc', 'desc' ] as const;
-export type QuerySortFlags = ( typeof QuerySortFlags )[ number ];
+export const QuerySortFlag = [ 1, -1, 'asc', 'desc' ] as const;
+export type QuerySortFlag = ( typeof QuerySortFlag )[ number ];
 
 // Type representing sorting options in a query
 export type QuerySortOptions = {
-    [ P in DataBasePaths ]?: QuerySortFlags;
+    [ P in DataBasePaths ]?: QuerySortFlag;
 };
