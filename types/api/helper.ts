ec911557f2d59aaf415a59449e7457b183e1667f
komed3 <pk97@outlook.de>
2025-10-19 00:07:49 +0200

    Update helper.ts

--- types/api/helper.ts
+++ types/api/helper.ts
@@ -2,6 +2,9 @@ import { Property } from '../abstract/property';
 import { Primitive } from '../abstract/utils';
 import { DataBase } from '../database';
 
+// Check if a Select-Object S contains any `true` value
+export type HasTrue< S > = Extract< S[ keyof S & ( string | number ) ], true > extends never ? false : true;
+
 // Value type of a Collection
 export type CollectionValue< T > = T extends Record< string, infer V > ? V : never;
 
