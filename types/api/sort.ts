f19ed5a12ef812a65663ff6436f46071f45a1a84
komed3 <pk97@outlook.de>
2025-10-18 13:21:55 +0200

    Update sort.ts

--- types/api/sort.ts
+++ types/api/sort.ts
@@ -1,8 +1,12 @@
-import { QueryableCollection } from './path';
+import { QueryableCollection } from './helper';
+import { CollectionPaths } from './path';
 
+// Sort flags
 export const QuerySortFlag = [ 1, -1, 'asc', 'desc' ] as const;
 export type QuerySortFlag = ( typeof QuerySortFlag )[ number ];
 
-export type QuerySortOptions = [
-    QueryableCollection, '', QuerySortFlag
-][];
+// Sort options
+export type QuerySortOptions = {
+    [ K in QueryableCollection & string ]:
+        [ K, CollectionPaths[ K ], QuerySortFlag ];
+}[ QueryableCollection & string ][];
