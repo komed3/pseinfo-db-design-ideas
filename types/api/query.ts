522fc8d1224abe3e2f4d43aa851fb0a1f8f8d648
komed3 <pk97@outlook.de>
2025-10-18 11:15:21 +0200

    Update query.ts

--- types/api/query.ts
+++ types/api/query.ts
@@ -1,7 +1,8 @@
+import { QueryableCollection } from './path';
 import { QuerySortOptions } from './sort';
 
 export type Query = {
-    $from: any; // not yet implemented
+    $from: QueryableCollection[];
     $select: any; // not yet implemented
     $where?: any; // not yet implemented
     $sort?: QuerySortOptions;
