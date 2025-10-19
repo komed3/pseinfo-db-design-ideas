8d59fe5e7f86a51280a672c62bfeb51ee1a26372
komed3 <pk97@outlook.de>
2025-10-19 09:23:41 +0200

    Update option.ts

--- types/api/option.ts
+++ types/api/option.ts
@@ -1,5 +1,13 @@
+import { PhysicalQuantity } from '../abstract/unit';
+
+// Allow specifying units for different physical quantities
+type UnitOptions = {
+    [ U in PhysicalQuantity ]?: string[];
+};
+
+// Options for query execution
 export type QueryOptions = {
     resolveRefs?: boolean;
     resolveUnits?: boolean;
-    units?: any; // not yet implemented
+    units?: UnitOptions;
 };
