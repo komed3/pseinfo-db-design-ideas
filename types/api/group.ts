ac59d747a801be4e5dc42142312e3ee662cffabb
komed3 <pk97@outlook.de>
2025-10-19 09:56:20 +0200

    implement grouping options

--- types/api/group.ts
+++ types/api/group.ts
@@ -1,1 +1,13 @@
-export type QueryGroupOptions = {};
+import { QueryFilter } from './filter';
+import { PathWalker } from './path';
+
+// Aggregation functions for grouping
+export const QueryAggregationFn = [ 'count', 'max', 'min', 'sum', 'avg' ] as const;
+export type QueryAggregationFn = ( typeof QueryAggregationFn )[ number ];
+
+// Type for grouping options in queries
+export type QueryGroupOptions = {
+    groupBy: PathWalker;
+    aggregate?: PathWalker< QueryAggregationFn >;
+    having?: QueryFilter;
+};
